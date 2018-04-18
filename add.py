d7e66d6c70a056f32c650031a2d37002d877f42a
SAYOOJ A P <36416@fisat>
2018-04-18 01:19:38 -0400

    create a fn to add 4 no

--- add.py
+++ add.py
@@ -4,6 +4,9 @@ def add2(num1,num2):
 def add3(num1,num2,num3):
 	r=num1+num2+num3
 	return r
-
+def add4(num1,num2,num3,num4):
+	q=num1+num2+num3+num4
+	return q
 print add2(3,4)
 print add3(4,5,6)
+print add4(1,2,3,4)
